bb2e7fad645a470ead5119f5f0cbb6fa0329789c
Cole Green <17110935+nox7@users.noreply.github.com>
2023-03-03 19:15:21 -0600

    OOP for Player + PlayerSkill

---
+++ DagothUrDiscordBot/StatFetcher/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DagothUrDiscordBot.StatFetcher
+{
+    internal class Player
+    {
+        private string name;
+        private int totalLevel;
+        private int totalXP;
+        private List<PlayerSkill> skillList;
+
+        public Player(string name, int totalLevel, int totalXP, List<PlayerSkill> skillList)
+        {
+            this.name = name;
+            this.totalLevel = totalLevel;
+            this.totalXP = totalXP;
+            this.skillList = skillList;
+        }
+
+        public int GetTotalLevel()
+        {
+            return this.totalLevel;
+        }
+
+        public string ToStringForDiscordWithoutSkills()
+        {
+            return $"**{this.name}**\n-- Total level: {this.totalLevel:n0}\n-- Total XP: {this.totalXP:n0}";
+        }
+    }
+}
---
+++ DagothUrDiscordBot/StatFetcher/PlayerSkill.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DagothUrDiscordBot.StatFetcher
+{
+    internal class PlayerSkill
+    {
+        private string name;
+        private int level;
+        private int xp;  
+
+        public PlayerSkill(string name, int level, int xp) {
+            this.name = name;
+            this.level = level; 
+            this.xp = xp; 
+        }
+    }
+}
