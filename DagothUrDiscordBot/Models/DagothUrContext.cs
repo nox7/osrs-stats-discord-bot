bc75da8d09eae49963f31727795eafdd249d4c52
Cole Green <17110935+nox7@users.noreply.github.com>
2023-03-26 13:44:45 -0500

    New scaffolding for modeling Player and PlayerSkill SQL

--- DagothUrDiscordBot/Models/DagothUrContext.cs
+++ DagothUrDiscordBot/Models/DagothUrContext.cs
@@ -17,22 +17,16 @@ public partial class DagothUrContext : DbContext
 
     public virtual DbSet<Player> Players { get; set; }
 
+    public virtual DbSet<PlayerSkill> PlayerSkills { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("dbConnection"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Player>(entity =>
+        modelBuilder.Entity<PlayerSkill>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Players__3214EC0782630384");
-
-            entity.Property(e => e.Id).ValueGeneratedNever();
-            entity.Property(e => e.LastDataFetchTimestamp).HasColumnName("last_data_fetch_timestamp");
-            entity.Property(e => e.MemberId).HasColumnName("member_id");
-            entity.Property(e => e.PlayerName)
-                .HasMaxLength(128)
-                .HasColumnName("player_name");
+            entity.HasOne(d => d.Player).WithMany(p => p.PlayerSkills).HasConstraintName("FK_PlayerSkills_Players");
         });
 
         OnModelCreatingPartial(modelBuilder);
