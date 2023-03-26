a74abb04cf588dcbfe228593acfcce7f8ead05c0
Cole Green <17110935+nox7@users.noreply.github.com>
2023-03-26 13:45:14 -0500

    Create ChangedSkill.cs

---
+++ DagothUrDiscordBot/PlayerSkillMonitor/ChangedSkill.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DagothUrDiscordBot.PlayerSkillMonitor
+{
+    internal class ChangedSkill
+    {
+        public string Name { get; set; }
+        public int DeltaLevel{ get; set; }
+        public int NewLevel{ get; set; }
+
+        public ChangedSkill(string name, int deltaLevel, int newLevel)
+        {
+            Name = name;
+            DeltaLevel = deltaLevel;
+            NewLevel = newLevel;
+        }
+    }
+}
