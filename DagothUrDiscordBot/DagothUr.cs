81ffa782b7e8f39e8888f0b0ab4ec9cd32e74ae1
Cole Green <17110935+nox7@users.noreply.github.com>
2024-08-18 08:51:44 -0500

    Modernize main class code and variables

--- DagothUrDiscordBot/DagothUr.cs
+++ DagothUrDiscordBot/DagothUr.cs
@@ -7,31 +7,32 @@ using Discord.Rest;
 using Discord.WebSocket;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 namespace DagothUrDiscordBot;
 
 class DagothUr
 {
-    private static DagothUr? instance;
+    private static DagothUr? ApplicationInstance;
 
-    private readonly DiscordSocketClient client;
-    private CommandManager commandManager;
-    private string _runtimeEnvironment;
-    private System.Timers.Timer? _skillChangeCheckTimer = null;
-    private DateTime _lastTimerTick = DateTime.Now;
-    private int _timerTickIntervalMs = 600000;
+    private readonly DiscordSocketClient DiscordClient;
+    private CommandManager CommandManager;
+    private string RuntimeEnvironment;
+    private System.Timers.Timer? SkillChangeCheckTimer = null;
+    private DateTime LastTimerTick = DateTime.Now;
+    private int TimerTickIntervalMs = 600000;
 
     static void Main(String[] args)
     {
-        instance = new DagothUr();
-        instance.MainAsync()
+        ApplicationInstance = new DagothUr();
+        ApplicationInstance.MainAsync()
             .GetAwaiter()
             .GetResult();
     }
 
     public static DagothUr GetInstance()
     {
-        return instance!;
+        return ApplicationInstance!;
     }
 
     public DagothUr()
@@ -43,30 +44,28 @@ class DagothUr
             throw new Exception("Missing environment variable 'RUNNING_ENVIRONMENT' - Use a value of either 'Development' or 'Production'");
         }
 
-        this._runtimeEnvironment = runtimeEnvironment;
+        RuntimeEnvironment = runtimeEnvironment;
 
         // Run migrations in Production
-        if (this.IsProduction())
+        if (IsProduction())
         {
             Console.WriteLine("Running database migrations.");
-            using (var dbContext = new OSRSStatBotDbContext())
-            {
-                dbContext.Database.Migrate();
-            }
+            using var dbContext = new OSRSStatBotDbContext();
+            dbContext.Database.Migrate();
         }
 
         // Setup the privileges
-        DiscordSocketConfig config = new DiscordSocketConfig
+        DiscordSocketConfig config = new()
         {
-            GatewayIntents = Discord.GatewayIntents.AllUnprivileged | Discord.GatewayIntents.MessageContent
+            GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
         };
 
         // Create the discord client
-        client = new DiscordSocketClient(config);
-        client.Log += OnLog;
-        client.Ready += OnReady;
+        DiscordClient = new DiscordSocketClient(config);
+        DiscordClient.Log += OnLog;
+        DiscordClient.Ready += OnReady;
 
-        commandManager = new CommandManager(client);
+        CommandManager = new CommandManager(DiscordClient);
     }
 
     /// <summary>
@@ -75,27 +74,35 @@ class DagothUr
     /// <returns></returns>
     public string GetRuntimeEnvironment()
     {
-        return this._runtimeEnvironment;
+        return RuntimeEnvironment;
     }
 
     public bool IsDevelopment()
     {
-        return this.GetRuntimeEnvironment().ToLower() == "development";
+        return GetRuntimeEnvironment().ToLower() == "development";
     }
 
     public bool IsProduction()
     {
-        return this.GetRuntimeEnvironment().ToLower() == "production";
+        return GetRuntimeEnvironment().ToLower() == "production";
     }
 
+    /// <summary>
+    /// The last DateTime the timer for checking stats ticked.
+    /// </summary>
+    /// <returns></returns>
     public DateTime GetLastTimerTickDateTime()
     {
-        return this._lastTimerTick;
+        return LastTimerTick;
     }
 
+    /// <summary>
+    /// Returns the millisecond interval that the skill check timer ticks at
+    /// </summary>
+    /// <returns></returns>
     public int GetSkillCheckTimerIntervalInMs()
     {
-        return this._timerTickIntervalMs;
+        return TimerTickIntervalMs;
     }
 
     /// <summary>
@@ -103,144 +110,161 @@ class DagothUr
     /// </summary>
     /// <param name="guildId"></param>
     /// <returns></returns>
-    public ulong? GetBotChatChannelForGuild(ulong guildId)
+    public async Task<ulong?> GetBotChatChannelForGuild(ulong guildId)
     {
-        using (var dbContext = new OSRSStatBotDbContext())
-        {
-            GuildDefaultChatChannel? chatChannel = dbContext.GuildDefaultChatChannels.Where(channel => channel.GuildId == guildId).FirstOrDefault();
-            if (chatChannel != null)
-            {
-                return chatChannel.ChannelId;
-            }
-        }
+        using var dbContext = new OSRSStatBotDbContext();
+        GuildDefaultChatChannel? chatChannel = await dbContext.GuildDefaultChatChannels
+            .Where(channel => channel.GuildId == guildId)
+            .FirstOrDefaultAsync();
 
-        return null;
+        return chatChannel?.ChannelId;
     }
 
+    /// <summary>
+    /// Starts the Discord client. Begins the timer for checking player changed events.
+    /// </summary>
+    /// <returns></returns>
     public async Task MainAsync()
     {
         string discordBotToken = Environment.GetEnvironmentVariable("token") ?? string.Empty;
-        await client.LoginAsync(TokenType.Bot, discordBotToken);
+        await DiscordClient.LoginAsync(TokenType.Bot, discordBotToken);
 
         // Starts the connection. Returns after one is established and runs the connection on another thread
-        await client.StartAsync();
+        await DiscordClient.StartAsync();
 
         // Setup a timer to recurringly check for player skill gains
         System.Timers.Timer playerSkillChangesTimer = new()
         {
-            Interval = _timerTickIntervalMs,
+            Interval = TimerTickIntervalMs,
             AutoReset = false,
             Enabled = true
         };
 
         playerSkillChangesTimer.Elapsed += OnPlayerSkillCheckTimedEvent;
-        _skillChangeCheckTimer = playerSkillChangesTimer;
+        SkillChangeCheckTimer = playerSkillChangesTimer;
 
         // Block the program from closing until it is closed manually
         await Task.Delay(Timeout.Infinite);
     }
 
+    /// <summary>
+    /// Fires when the skill check timer elapses.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="e"></param>
     private async void OnPlayerSkillCheckTimedEvent(Object? source, System.Timers.ElapsedEventArgs e)
     {
-        this._lastTimerTick = DateTime.Now;
         Console.WriteLine("Skill check timer ticked.");
+        LastTimerTick = DateTime.Now;
         var statFetcher = new StatFetcher();
 
         // Fetch all servers that have given a default chat channel
-        using (var dbContext = new OSRSStatBotDbContext())
+        using var dbContext = new OSRSStatBotDbContext();
+
+        // Handle 100 players at a time to avoid server strain on both our DB and Jagex's
+        int currentPage = 1;
+        int limit = 100;
+
+        while(true)
         {
-            // Handle 100 players at a time to avoid server strain on both our DB and Jagex's
-            int currentPage = 1;
-            int limit = 100;
+            List<Player> trackedPlayers = await dbContext.Players
+                .Skip((currentPage - 1) * limit)
+                .Take(limit)
+                .ToListAsync();
 
-            while(true)
+            if (trackedPlayers.Count == 0)
             {
-                List<Player> trackedPlayers = dbContext.Players.Skip((currentPage - 1) * limit).Take(limit).ToList();
+                Debug.WriteLine($"Done reading tracked players in timer on page {currentPage}");
+                // Break when the list is empty
+                break;
+            }
 
-                if (trackedPlayers.Count == 0)
+            // Lookup live stats
+            foreach(Player dbPlayer in trackedPlayers)
+            {
+                HiscoresPlayer? hsPlayer = await statFetcher.GetHiscoresPlayerFromRSN(dbPlayer.PlayerName);
+                if (hsPlayer != null)
                 {
-                    Debug.WriteLine($"Done reading tracked players in timer on page {currentPage}");
-                    // Break when the list is empty
-                    break;
-                }
-
-                // Lookup live stats
-                foreach(Player dbPlayer in trackedPlayers)
-                {
-                    HiscoresPlayer? hsPlayer = await statFetcher.GetHiscoresPlayerFromRSN(dbPlayer.PlayerName);
-                    if (hsPlayer != null)
+                    List<ChangedSkill> changedSkills = dbPlayer.GetAnyChangedSkillsComparedToList(hsPlayer.GetSkillList());
+                    if (changedSkills.Count > 0)
                     {
-                        List<ChangedSkill> changedSkills = dbPlayer.GetAnyChangedSkillsComparedToList(hsPlayer.GetSkillList());
-                        if (changedSkills.Count > 0)
+                        Debug.WriteLine($"{dbPlayer.PlayerName} has skill changes.");
+                        // Has changed skills
+                        dbPlayer.LastDataFetchTimestamp = (int) DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+                        dbPlayer.TotalLevel = hsPlayer.GetTotalLevel();
+                        dbPlayer.TotalXP = hsPlayer.GetTotalXP();
+
+                        var changedPlayer = new ChangedPlayer
                         {
-                            Debug.WriteLine($"{dbPlayer.PlayerName} has skill changes.");
-                            // Has changed skills
-                            dbContext.Attach(dbPlayer);
-                            dbPlayer.LastDataFetchTimestamp = (int) DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-                            dbPlayer.TotalLevel = hsPlayer.GetTotalLevel();
-                            dbPlayer.TotalXP = hsPlayer.GetTotalXP();
+                            player = dbPlayer,
+                            changedSkills = changedSkills
+                        };
 
-                            var changedPlayer = new ChangedPlayer();
-                            changedPlayer.player = dbPlayer;
-                            changedPlayer.changedSkills = changedSkills;
+                        var embedForChangedPlayer = changedPlayer.GetDiscordEmbedRepresentingChanges();
 
-                            var embedForChangedPlayer = changedPlayer.GetDiscordEmbedRepresentingChanges();
+                        // Update their stats in the DB
+                        dbPlayer.UpdateSkills(hsPlayer.GetSkillList());
 
-                            // Update their stats in the DB
-                            dbPlayer.UpdateSkills(hsPlayer.GetSkillList());
+                        // Fetch the guild ID for this player
+                        List<ulong> guildIDsPlayerIsMonitoredBy = PlayerGuildLink.GetAllGuildIdsPlayerIsMonitoredBy(dbPlayer.Id);
 
-                            // Fetch the guild ID for this player
-                            List<ulong> guildIDsPlayerIsMonitoredBy = PlayerGuildLink.GetAllGuildIdsPlayerIsMonitoredBy(dbPlayer.Id);
+                        foreach(ulong guildId in guildIDsPlayerIsMonitoredBy)
+                        {
+                            SocketGuild guild = DiscordClient.GetGuild(guildId);
+                            Debug.WriteLine($"Found guild {guildId} for player {dbPlayer.PlayerName}.");
 
-                            foreach(ulong guildId in guildIDsPlayerIsMonitoredBy)
+                            // Did they set a bot text channel?
+                            ulong? channelId = await GetBotChatChannelForGuild(guildId);
+                            if (channelId != null)
                             {
-                                SocketGuild guild = client.GetGuild(guildId);
-                                Debug.WriteLine($"Found guild {guildId} for player {dbPlayer.PlayerName}.");
-
-                                // Did they set a bot text channel?
-                                ulong? channelId = GetBotChatChannelForGuild(guildId);
-                                if (channelId != null)
-                                {
-                                    SocketTextChannel textChannel = guild.GetTextChannel((ulong)channelId);
-                                    RestUserMessage responseMessage = await textChannel.SendMessageAsync(embed: embedForChangedPlayer);
-                                    Debug.WriteLine($"Message sent for {guildId} and {channelId}.");
-                                }
-                                else
-                                {
-                                    Console.WriteLine($"Guild {guildId} did not set a default bot channel to post about {dbPlayer.PlayerName}'s skill updates.");
-                                }
+                                SocketTextChannel textChannel = guild.GetTextChannel((ulong)channelId);
+                                RestUserMessage responseMessage = await textChannel.SendMessageAsync(embed: embedForChangedPlayer);
+                                Debug.WriteLine($"Message sent for {guildId} and {channelId}.");
                             }
-                        }
-                        else
-                        {
-                            Debug.WriteLine($"{dbPlayer.PlayerName} has no skill changes to post about.");
+                            else
+                            {
+                                Console.WriteLine($"Guild {guildId} did not set a default bot channel to post about {dbPlayer.PlayerName}'s skill updates.");
+                            }
                         }
                     }
                     else
                     {
-                        // TODO Handle failures with DB counter and remove from the DB if the counter has 3 failures
-                        Console.WriteLine($"Error looking up {dbPlayer.PlayerName} - did their RSN change?");
+                        Debug.WriteLine($"{dbPlayer.PlayerName} has no skill changes to post about.");
                     }
                 }
-
-                dbContext.SaveChanges();
-                ++currentPage;
+                else
+                {
+                    // TODO Handle failures with DB counter and remove from the DB if the counter has 3 failures
+                    Console.WriteLine($"Error looking up {dbPlayer.PlayerName} - did their RSN change?");
+                }
             }
 
+            await dbContext.SaveChangesAsync();
+            ++currentPage;
         }
 
         // After all done, start the timer again
-        _skillChangeCheckTimer.Start();
+        SkillChangeCheckTimer!.Start();
     }
 
+    /// <summary>
+    /// Fires when the Discord client logs a message.
+    /// </summary>
+    /// <param name="log"></param>
+    /// <returns></returns>
     private Task OnLog(LogMessage log)
     {
         Console.WriteLine(log.ToString());
         return Task.CompletedTask;
     }
 
+    /// <summary>
+    /// Fires when the Discord client has successfully started and is ready to process commands and receive
+    /// data.
+    /// </summary>
+    /// <returns></returns>
     private async Task OnReady()
     {
-        await commandManager.RegisterCommands();
+        await CommandManager.RegisterCommands();
     }
 }
